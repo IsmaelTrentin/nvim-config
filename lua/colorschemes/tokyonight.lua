c6dc2417bc362f9defbe252231c6fd363c39a431
IsmaelTrentin <ismaeltrentin@gmail.com>
2025-09-08 18:01:49 +0200

    theme: update purple and telescope colors

--- lua/colorschemes/tokyonight.lua
+++ lua/colorschemes/tokyonight.lua
@@ -4,6 +4,26 @@ return {
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
+    require('tokyonight').setup {
+      style = 'moon',
+      transparent = false,
+
+      on_colors = function(colors)
+        colors.bg = '#1a1b26'
+        colors.bg_float = '#1c1e2d'
+        colors.purple = '#ae81ff'
+
+        -- colors.bg_dark = '#1a1b26'
+      end,
+
+      on_highlights = function(hl, c)
+        local colors = require('tokyonight.colors').setup()
+
+        hl.TelescopePromptBorder.fg = colors.purple
+        hl.TelescopePromptTitle.fg = colors.purple
+      end,
+    }
+
     vim.cmd.colorscheme 'tokyonight-moon'
     vim.cmd.hi 'Comment gui=none'
   end,
