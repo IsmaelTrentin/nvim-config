1312b43b8c1abe2057ce2503a353411f87ecdb28
IsmaelTrentin <pandux66@gmail.com>
2025-10-20 11:55:46 +0200

    fix(ui): replace list tab char

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,11 +1,18 @@
 -- indent lines
+local IBL_CHAR = '│'
+
 return {
   'lukas-reineke/indent-blankline.nvim',
   main = 'ibl',
   ---@module "ibl"
   ---@type ibl.config
   opts = {
-    indent = { char = '│' },
+    -- nvim 'list' replaces tab_char
+    --  but `set nolist` has weird behaviour with col0
+    indent = { char = IBL_CHAR, tab_char = IBL_CHAR },
+    -- disable styling to get a slimmer bar
+    -- and make it consistent to the nvim list tab char
+    whitespace = { highlight = {} },
   },
   config = function()
     require('ibl').setup()
